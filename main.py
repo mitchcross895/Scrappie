bab5523778fa52c4f4c1af7644b72935117e33c0
Mitchel Cross <mitch.p.cross@gmail.com>
2025-03-31 01:13:12 -0500

    added a 'load_dotenv()' line

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ from discord import app_commands
 logging.basicConfig(level=logging.INFO)
 
 # Load environment variables
+
+load_dotenv()
+
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
